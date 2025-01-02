dabf2ff52e36d917b8ad096a52ad0b89cdb948a1
Hasan <sohagali.ru.ac@gmail.com>
2025-01-02 11:25:11 +0600

    create category query and change readme

---
+++ dist/gql/resolver/index.js
@@ -0,0 +1,15 @@
+import { db } from "../../db.js";
+export const resolvers = {
+    Query: {
+        products: () => db.products,
+        product: (parent, args, context) => {
+            const result = db.products.find((item) => item.id === args.productId);
+            return result;
+        },
+        categories: () => db.categories,
+        category: (parent, args, context) => {
+            const result = db.categories.find((item) => item.id === args.categoryId);
+            return result;
+        },
+    },
+};
