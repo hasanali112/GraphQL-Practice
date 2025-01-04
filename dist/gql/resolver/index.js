4467798db2d64bbfb2f7a0903235b3be67f12043
Hasan <sohagali.ru.ac@gmail.com>
2025-01-04 06:48:29 +0600

    create one to many relationship

--- dist/gql/resolver/index.js
+++ dist/gql/resolver/index.js
@@ -3,19 +3,21 @@ export const resolvers = {
     Query: {
         products: () => db.products,
         product: (parent, args, context) => {
-            const result = db.products.find((item) => item.id === args.productId);
-            return result;
+            return db.products.find((item) => item.id === args.productId);
         },
         categories: () => db.categories,
         category: (parent, args, context) => {
-            const result = db.categories.find((item) => item.id === args.categoryId);
-            return result;
+            return db.categories.find((item) => item.id === args.categoryId);
         },
     },
     Product: {
         category: (parent, args, context) => {
-            const result = db.categories.find((item) => item.id === parent.categoryId);
-            return result;
+            return db.categories.find((item) => item.id === parent.categoryId);
+        },
+    },
+    Category: {
+        products: (parent, args, context) => {
+            return db.products.filter((item) => item.categoryId === parent.id);
         },
     },
 };
